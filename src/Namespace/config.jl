4b29239d2a62f735769b07daccc5def6c4c3a826
Yujie <jesiner@gmail.com>
2025-11-25 12:17:30 +0800

    move config info and features settings

--- src/Namespace/config.jl
+++ src/Namespace/config.jl
@@ -2,10 +2,14 @@
 General configuration for the SPAC model.
 """
 Base.@kwdef mutable struct SPACConfig{FT<:AbstractFloat}
+    "General configuration information"
+    CONFIG_INFO::SPACConfigInfo = SPACConfigInfo()
     "Constants used in the SPAC model"
-    CONSTANTS::SPACConstants{FT} = SPACConstants{FT}();
+    CONSTANTS::SPACConstants{FT} = SPACConstants{FT}()
     "Dimensions of the SPAC system"
-    DIMENSIONS::SPACDimensions{FT} = SPACDimensions{FT}();
+    DIMENSIONS::SPACDimensions{FT} = SPACDimensions{FT}()
+    "Features on/off/settings of the SPAC model"
+    FEATURES::SPACFeatures = SPACFeatures()
     "Methods used in the SPAC model"
     METHODS::SPACMethods{FT} = SPACMethods{FT}()
 end;
